113cd1e7b0bf024b4790d119b29fef6ae38dd3e6
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-03-02 13:33:59 -0800

    Add files via upload

---
+++ data/EmployeeSQL/schema.sql
@@ -0,0 +1,84 @@
+-- Create tables and import data
+-- Drop table if exists
+DROP TABLE IF EXISTS departments;
+
+-- Create new table
+CREATE TABLE departments (
+  dept_no VARCHAR,
+  dept_name VARCHAR,
+);
+
+-- Verify successful data import
+SELECT * FROM departments;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_emp;
+
+-- Create new table
+CREATE TABLE dept_emp (
+  emp_no INT,
+  dept_no VARCHAR,
+  from_date DATE,
+  to_date DATE
+);
+
+-- Verify successful data import
+SELECT * FROM dept_emp;
+
+
+-- Drop table if exists
+DROP TABLE IF EXISTS dept_manager;
+
+-- Create new table
+CREATE TABLE dept_manager (
+  dept_no VARCHAR,
+  emp_no INT,
+  from_date DATE,
+  to_date DATE
+);
+
+SELECT * FROM dept_manager
+
+-- Drop if exists
+DROP TABLE IF EXISTS employees;
+
+-- Create new table
+CREATE TABLE employees (
+  emp_no INT,
+  birth_date DATE,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  gender VARCHAR,
+  hire_date DATE
+);
+
+-- Verify successful data import
+SELECT * FROM employees;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS salaries;
+
+-- Create new table
+CREATE TABLE salaries (
+  emp_no INT,
+  salary INT,
+  from_date DATE,
+  to_date DATE
+);
+
+-- Verify successful data import
+SELECT * FROM salaries;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS titles;
+
+-- Create new table
+CREATE TABLE titles (
+  emp_no INT,
+  title VARCHAR,
+  from_date DATE,
+  to_date DATE
+);
+
+-- Verify successful data import
+SELECT * FROM titles;
